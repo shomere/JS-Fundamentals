c9d4ea989e0a498eb36aa748c1105b4dab2e27e0
shomere <s.homere@alustudent.com>
2025-09-15 16:04:25 +0200

    1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,4 @@
-let myLetter = ["C is fun", "Python is cool, "JavaScript is amazing"];
+let myLetter = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for(i =0; i < myLetter.length; i++){
 	console.log(myLetter[i];
 }
