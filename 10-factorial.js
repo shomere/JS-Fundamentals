c7cbb36a3326e04f5879afd15ddf2a7601706961
shomere <s.homere@alustudent.com>
2025-09-15 20:08:53 +0200

    10-factorial.js

---
+++ 10-factorial.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+
+function factorial(n) {
+    if (isNaN(n) || n <= 0) return 1;
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(num));
